87bb93791a8390e543d6a48616cc1255fb6be098
martin <martin.me15@yahoo.com>
2014-04-07 01:41:30 +0800

    fixing invalid variable name

--- himignu/app/controllers/informations_controller.rb
+++ himignu/app/controllers/informations_controller.rb
@@ -2,13 +2,19 @@ class InformationsController < ApplicationController
 
   def show
     @information_selected = "selected"
-    @event = "empty"
-    @informations = Information.other_informations.select([:name, :permalink])
+    @information = "empty"
+    @informations = Information.other_informations
     unless params[:id] == "empty"
       @information = Information.find_by_permalink(params[:id])
       if @information.nil?
-        @information = Information.first || "empty"
         flash[:error] = "Cannot find Information with name '#{params[:id]}'"
+	if @informations.first.nil?
+          @information = "empty"
+	  redirect_to information_path(@information)
+        else
+          @information = @informations.first
+	  redirect_to information_path(@information.permalink)
+        end
       end
     end
   end
