2b5919fc8c1e45d23504cdf4f9c5e097838286e0
martin <martin.me15@yahoo.com>
2014-04-07 00:13:54 +0800

    fixing when porpular info is empty

--- himignu/app/controllers/application_controller.rb
+++ himignu/app/controllers/application_controller.rb
@@ -5,8 +5,8 @@ class ApplicationController < ActionController::Base
 
   private
     def load_default_data
-      @data_info = Information.where("is_popular = 1").limit(1)[0].permalink
-      @data_info = "empty" if @data_info.empty?
+      @data_info = Information.where("is_popular = 1").limit(1)
+      @data_info = @data_info.empty? ? "empty" : @data_info[0].permalink
       @data_event = Event.where("name = 'MKI'")[0].permalink || "empty"
       @data_event = "empty" if @data_event.empty?
     end
