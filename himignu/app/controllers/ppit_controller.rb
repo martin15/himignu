d165b79e5997812a12833405899700612ade40fa
Martin <root@loveserver.(none)>
2014-05-16 19:12:04 +0400

    edited contact us, ppit title

--- himignu/app/controllers/ppit_controller.rb
+++ himignu/app/controllers/ppit_controller.rb
@@ -3,6 +3,6 @@ class PpitController < ApplicationController
   def index
     @ppit_selected = "selected"
     @bg_color = "bg-coklat-muda.jpg"
-    @ppit = Information.find_by_name("PPIT")
+    @ppit = Information.find_by_permalink("PPIT")
   end
 end
