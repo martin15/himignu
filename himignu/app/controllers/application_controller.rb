97ad511756e6c28735a95e8f969599b7d9e3615b
martin <martin.me15@yahoo.com>
2014-12-22 15:04:57 +0800

    handling invalid params page and change banner order

--- himignu/app/controllers/application_controller.rb
+++ himignu/app/controllers/application_controller.rb
@@ -1,6 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :load_default_data
+  before_filter :params_validation
+
+  def params_validation
+    unless params[:page].to_s != "0" && (params[:page].nil? || /^\d+$/ =~ params[:page])
+      respond_to do |format|
+        format.html { render :file => "#{Rails.root}/public/404",
+                             :layout => false, :status => :not_found }
+      end
+    end
+  end
 
 
   private
--- himignu/app/controllers/home_controller.rb
+++ himignu/app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
   def index
     @home_selected = "selected"
     @bg_color = "bg-merah.jpg"
-    @banners = Banner.all
+    @banners = Banner.order("updated_at DESC")
     @events = Event.main_events.shuffle.sample(7)
     @popular_info = Information.where("is_popular = 1").limit(3)
     @short_ppit = Information.find_by_name("Short PPIT")
